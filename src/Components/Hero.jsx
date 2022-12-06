eb3819e056f0613f35cf4dd9887b2c59a64f7c92
kevinzepwa <zepwakevin8@gmail.com>
2022-12-07 00:05:14 +0300

    ft(home bg)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import bgImg from '../assets/cyber-bg.png'
 
 function Hero() {
   return (
-    <div name='home' className='w-full h-screen flex flex-col justify-between sm:bg-zinc-200 bg-blue-100'>
+    <div name='home' id='home' className='w-full bg-bgImg h-screen flex flex-col justify-between sm:bg-zinc-200 bg-blue-100'>
         <div className='my-32 grid text-center max-w-[1240px] m-auto'>
             <div className='flex flex-col justify-center w-full px-2 py-8'>
-                <h1 className='py-3 font-bold sm:text-slate-600 text-yellow-800 text-center'>Art is beautiful, <br/> Art is life</h1>
-                <p className='text-slate-600 text-center'>We unite art with its next owner</p>
+                <h1 className='py-3 font-bold text-white text-center'>Art is beautiful, <br/> Art is life</h1>
+                <p className='text-slate-200 text-center'>We unite art with its next owner</p>
                 <button className='py-3 px-8  mt-12 mb-16 sm:px-5'>Explore</button>
             </div>       
         </div>
